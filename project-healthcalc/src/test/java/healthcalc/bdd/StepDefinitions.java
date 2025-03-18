622e7c4f8e5c3570915253cbd7f90debedfd38f1
Achraf <achraf.ousti.elmoussati@gmail.com>
2025-03-18 13:48:47 +0100

    Deploy Cucumber to the repository by updating the pom.xml

---
+++ project-healthcalc/src/test/java/healthcalc/bdd/StepDefinitions.java
@@ -0,0 +1,13 @@
+package healthcalc.bdd;
+
+import healthcalc.HealthCalcImpl;
+
+public class StepDefinitions {
+
+	
+	/*private HealthCalcImpl healthCalc;
+	private Integer result ;
+	private boolean raiseException;*/
+	
+	
+}
