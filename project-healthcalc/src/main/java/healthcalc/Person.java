8aae8774d1d7ee1101157eacab1281b207a1a824
Achraf <achraf.ousti.elmoussati@gmail.com>
2025-05-13 13:38:02 +0200

    Add Interface Person

---
+++ project-healthcalc/src/main/java/healthcalc/Person.java
@@ -0,0 +1,10 @@
+package healthcalc;
+
+public interface Person {
+
+    public float weight() throws Exception;
+    public float height() throws Exception;
+    public Gender gender() throws Exception;
+    public int age() throws Exception;
+    
+}
