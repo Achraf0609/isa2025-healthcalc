48f806316a3e72b413ba45eb93059e1142d7beba
Achraf <achraf.ousti.elmoussati@gmail.com>
2025-05-20 14:13:55 +0200

    fix: Decorator Pattern Diagram

--- project-healthcalc/src/main/java/healthcalc/IdiomaDecorator.java
+++ project-healthcalc/src/main/java/healthcalc/IdiomaDecorator.java
@@ -7,6 +7,14 @@ public abstract class IdiomaDecorator implements  HealthHospital {
     public IdiomaDecorator(HealthHospital wrappe) {
         this.wrappee = wrappe;
     }
+    @Override
+    public double bmr(char genero, int edad, float altura, int peso) throws Exception {
+        return wrappee.bmr(genero, edad, altura, peso);
+    }
+    @Override
+    public int pesoIdeal(char genero, float altura) throws Exception {
+        return wrappee.pesoIdeal(genero, altura);
+    }
 
     
 }
--- project-healthcalc/src/main/java/healthcalc/RegionDecorator.java
+++ project-healthcalc/src/main/java/healthcalc/RegionDecorator.java
@@ -7,6 +7,14 @@ public abstract class RegionDecorator implements HealthHospital{
     public RegionDecorator(HealthHospital wrappe) {
         this.wrappee = wrappe;
     }
+    @Override
+    public double bmr(char genero, int edad, float altura, int peso) throws Exception {
+        return wrappee.bmr(genero, edad, altura, peso);
+    }
+    @Override
+    public int pesoIdeal(char genero, float altura) throws Exception {
+        return wrappee.pesoIdeal(genero, altura);
+    }
 
 
     
