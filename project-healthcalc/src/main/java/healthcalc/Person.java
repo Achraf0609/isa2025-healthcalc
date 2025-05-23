25d405058cfca9e2cad824031aa8b73f643a603e
Achraf <achraf.ousti.elmoussati@gmail.com>
2025-05-23 21:26:10 +0200

    Refactoring: Introduction of the 'Person' interface to replace multiple parameters

--- project-healthcalc/src/main/java/healthcalc/Person.java
+++ project-healthcalc/src/main/java/healthcalc/Person.java
@@ -4,7 +4,7 @@ public interface Person {
 
     public float weight() throws Exception;
     public float height() throws Exception;
-    public Gender gender() throws Exception;
     public int age() throws Exception;
+    public Gender gender() throws Exception;
     
 }
